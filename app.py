dfe6a4d21a2140dd0b59eb3ad817db7a82524d45
xuanthamnguyencao181027-create <xuanthamnguyencao181027@gmail.com>
2026-06-17 17:01:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.image("logo.jpg")
 # Tiêu đề app
-st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 2_TS. VŨ ĐỨC BÌNH")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 2_Nguyễn Cao Xuân Thẩm")
 
 # Nhập dữ liệu
 C = st.number_input(
